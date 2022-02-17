a35f8dae198abec98585f4845baceba05953b651
L1quid <dan@shup.com>
2022-02-17 18:14:51 +0000

    better console output

--- lib/critical-path-css-rails.rb
+++ lib/critical-path-css-rails.rb
@@ -8,15 +8,21 @@ module CriticalPathCss
 
   def self.generate(route)
     css = fetcher.fetch_route(route)
+    hsh = Digest::SHA256.hexdigest(route)
+    puts hsh + "\t" + route
     #::Rails.cache.write(route, css, namespace: CACHE_NAMESPACE, expires_in: nil)
-    fn = ::Rails.root.join(fetcher.config.out_dir, Digest::SHA256.hexdigest(route) + ".css")
+    fn = ::Rails.root.join(fetcher.config.out_dir, hsh + ".css")
+    puts fn
     File.open(fn, "w") {|f| f.write(css)}
   end
 
   def self.generate_all
     fetcher.fetch.each do |route, css|
       #::Rails.cache.write(route, css, namespace: CACHE_NAMESPACE, expires_in: nil)
-      fn = ::Rails.root.join(fetcher.config.out_dir, Digest::SHA256.hexdigest(route) + ".css")
+      hsh = Digest::SHA256.hexdigest(route)
+      puts hsh + "\t" + route
+      fn = ::Rails.root.join(fetcher.config.out_dir, hsh + ".css")
+      puts fn
       File.open(fn, "w") {|f| f.write(css)}
     end
   end
