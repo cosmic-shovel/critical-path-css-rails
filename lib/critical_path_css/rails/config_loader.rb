c61f1b53575076368ae9cb7db86bd18c9394adf2
L1quid <dan@shup.com>
2022-02-17 18:15:56 +0000

    fixing css path to read from disk

--- lib/critical_path_css/rails/config_loader.rb
+++ lib/critical_path_css/rails/config_loader.rb
@@ -22,9 +22,8 @@ module CriticalPathCss
         config['css_paths'] = [config['css_path']] if config['css_path']
 
         unless config['css_paths']
-          config['css_paths'] = [ActionController::Base.helpers.stylesheet_path(config['manifest_name'])]
+          config['css_paths'] = [ActionController::Base.helpers.stylesheet_path(config['manifest_name'], host: "#{::Rails.root}/public")]
         end
-        config['css_paths'].map! { |path| format_path(path) }
       end
 
       def format_path(path)
